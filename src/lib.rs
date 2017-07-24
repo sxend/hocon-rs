94dc6b01c888f0378f00ed026350cbc7a3a57d05
sxend <arimitsu.k+github@gmail.com>
2017-07-25 00:02:37 +0900

    add methods

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,67 @@
+use std::collections::*;
+use std::fs::File;
+use std::io::prelude::*;
 
 //pub mod parser;
 pub trait Config : std::fmt::Debug {
-    fn get_bool(&self) -> bool;
+    fn get_bool(&self, key: String) -> bool;
+    fn get_string(&self, key: String) -> String;
+    fn with_fallback(&self, config: &Config) -> Box<Config>;
+    fn to_map(&self) -> HashMap<String, String>;
 }
 
+
 #[derive(Debug)]
-struct ConfigImpl;
+struct ConfigImpl {
+    underlying: HashMap<String, String>
+}
 
 impl Config for ConfigImpl {
-    fn get_bool(&self) -> bool {
-        true
+    fn get_bool(&self, key: String) -> bool {
+        match self.underlying.get(&key).unwrap().as_ref() {
+            "true" => true,
+            _ => false
+        }
+    }
+    fn get_string(&self, key: String) -> String {
+        self.underlying.get(&key).map(|x| { x.to_string()}).unwrap()
+    }
+    fn to_map(&self) -> HashMap<String, String> {
+        self.underlying.clone()
+    }
+    fn with_fallback(&self, config: &Config) -> Box<Config> {
+        let mut new_underlying: HashMap<String, String> = HashMap::new();
+        for (key, value) in self.to_map().iter() {
+            new_underlying.insert(key.to_string(), value.to_string());
+        }
+        for (key, value) in config.to_map().iter() {
+            new_underlying.insert(key.to_string(), value.to_string());
+        }
+        Box::new(ConfigImpl{
+            underlying: new_underlying
+        })
     }
 }
 
 pub struct ConfigFactory;
 
 impl ConfigFactory {
+    pub fn parse_from_path(path: String) -> Box<Config> {
+        let mut config_string = String::new();
+        if let Ok(mut config_file) = File::open(path) {
+            config_file.read_to_string(&mut config_string).unwrap();
+        }
+        ConfigFactory::parse_from_string(config_string)
+    }
+    pub fn parse_from_string(config_string: String) -> Box<Config> {
+        let mut underlying: HashMap<String, String> = HashMap::new();
+        Box::new(ConfigImpl{
+            underlying: underlying
+        })
+    }
     pub fn load() -> Box<Config> {
-        Box::new(ConfigImpl{})
+        let path = "$HOME/application.conf"; // TODO: detect config directory
+        ConfigFactory::parse_from_path(path.to_string())
     }
 }
 
--- tests/lib.rs
+++ tests/lib.rs
@@ -3,7 +3,22 @@ extern crate hocon;
 use hocon::*;
 
 #[test]
-fn config_factory_load() -> () {
-    let config = ConfigFactory::load();
-    println!("{:?}", config);
+fn load_from_not_exists_path() -> () {
+    let config: Box<Config> = ConfigFactory::parse_from_path("".to_string());
+    assert_eq!(config.to_map().is_empty(), true);
+}
+
+#[test]
+fn parse_from_string() -> () {
+    let config_string = "\
+    foo {
+      bar {
+        fizz = fizz
+      }
+    }
+    foo.bar.buzz = buzz;
+    ".to_string();
+    let config: Box<Config> = ConfigFactory::parse_from_string(config_string);
+    assert_eq!(config.get_string("foo.bar.fizz".to_string()), "fizz");
+    assert_eq!(config.get_string("foo.bar.buzz".to_string()), "buzz");
 }
