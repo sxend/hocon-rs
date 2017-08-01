5f26322c52dfe17c823d239890de58ec5c321e4b
sxend <arimitsu.k+github@gmail.com>
2017-08-01 22:27:02 +0900

    reractor values

--- src/lib.rs
+++ src/lib.rs
@@ -3,59 +3,70 @@ use std::fs::File;
 use std::io::prelude::*;
 use std::io::Error;
 
-type BoxConfig = Box<Config>;
-
-pub trait Config : std::fmt::Debug {
-    fn get_bool(&self, key: &String) -> Option<bool>;
-    fn get_string(&self, key: &String) -> Option<String>;
-    fn get_config(&self, key: &String) -> Option<BoxConfig>;
+#[derive(Debug, Clone)]
+pub struct Config {
+    underlying: HashMap<String, ConfigValue>,
 }
 
-#[derive(Debug)]
-struct ConfigImpl {
-    underlying: HashMap<String, String>
+#[derive(Debug, Clone)]
+pub enum ConfigValue {
+    None,
+    String(String),
+    Bool(bool),
+    Config(Config)
 }
 
-impl Config for ConfigImpl {
-    fn get_bool(&self, key: &String) -> Option<bool> {
+impl ConfigValue {
+}
+
+impl Config {
+    pub fn get_bool(&self, key: &String) -> Option<bool> {
         match self.underlying.get(key) {
-            Some(x) if x == "true" => Some(true),
-            _ => Some(false)
+            Some(&ConfigValue::Bool(ref value)) => Some(*value),
+            _ => None
         }
     }
-    fn get_string(&self, key: &String) -> Option<String> {
-        self.underlying.get(key).map(|x| { x.to_string()})
+    pub fn get_string(&self, key: &String) -> Option<String> {
+        match self.underlying.get(key) {
+            Some(&ConfigValue::String(ref value)) => Some(value.to_owned()),
+            _ => None
+        }
     }
 
-    fn get_config(&self, key: &String) -> Option<BoxConfig> {
-        unimplemented!()
+    pub fn get_config(&self, key: &String) -> Option<Config> {
+        match self.underlying.get(key) {
+            Some(&ConfigValue::Config(ref value)) => Some(value.to_owned()),
+            _ => None
+        }
     }
 }
 
-pub struct ConfigFactory;
+pub struct ConfigFactory {
+}
 
 impl ConfigFactory {
-    pub fn parse_from_path(path: &String) -> Result<BoxConfig, Error> {
+    pub fn parse_from_path(path: &String) -> Result<Config, Error> {
         match File::open(path) {
             Ok(file) => ConfigFactory::parse_from_file(&file),
             Err(t) => Err(t)
         }
 
     }
-    pub fn parse_from_file(mut config_file: &File) -> Result<BoxConfig, Error> {
+    pub fn parse_from_file(mut config_file: &File) -> Result<Config, Error> {
         let mut config_string = String::new();
         match config_file.read_to_string(&mut config_string) {
             Ok(_) => ConfigFactory::parse_from_string(&config_string),
             Err(t) => Err(t)
         }
     }
-    pub fn parse_from_string(config_string: &String) -> Result<BoxConfig, Error> {
-        let mut underlying: HashMap<String, String> = HashMap::new();
-        Ok(Box::new(ConfigImpl{
+    pub fn parse_from_string(config_string: &String) -> Result<Config, Error> {
+        let mut underlying: HashMap<String, ConfigValue> = HashMap::new();
+        underlying.insert("foo.bar.fizz".to_string(), ConfigValue::String(config_string.to_owned())); // FIXME
+        Ok(Config {
             underlying: underlying
-        }))
+        })
     }
-    pub fn load() -> Result<BoxConfig, Error> {
+    pub fn load() -> Result<Config, Error> {
         let path = "$HOME/application.conf"; // TODO: resolve config path
         ConfigFactory::parse_from_path(&path.to_string())
     }
